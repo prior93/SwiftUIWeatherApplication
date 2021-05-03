bdbb70cb60d2fc5590f8b41756613506863d958b
prior93 <Parashar.adhikary@gmail.com>
2021-05-04 00:09:44 +0300

    Initial Commit

---
+++ SwiftUIWeatherApplication/SwiftUIWeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherApplicationApp.swift
+//  SwiftUIWeatherApplication
+//
+//  Created by parashar.r.adhikary on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
